8c591dd2759453c705b1b066ddbc374e15b98313
bhoinikhil <bhoinikk@gmail.com>
2026-04-24 12:21:03 +0530

    mapping completed

--- src/main/java/com/whatsApp/chatbot/serviceImpl/ChatbotServiceImpl.java
+++ src/main/java/com/whatsApp/chatbot/serviceImpl/ChatbotServiceImpl.java
@@ -1,4 +1,12 @@
 package com.whatsApp.chatbot.serviceImpl;
 
+import com.whatsApp.chatbot.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ChatbotServiceImpl {
+
+    @Autowired
+    MessageRepository messageRepository;
 }
